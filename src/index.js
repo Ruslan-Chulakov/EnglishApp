d3763ba255ea0cee066544eedeb76baca716b0af
Ruslan-Chulakov <AN_124_100@ukr.net>
2023-05-27 17:33:50 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter basename='/Eng-App'>
-      <App/>
-   </BrowserRouter>
-  </React.StrictMode>
+    <React.StrictMode>
+        <BrowserRouter basename="EnglishApp">
+            <App />
+        </BrowserRouter>
+    </React.StrictMode>
 );
